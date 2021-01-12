418dbca1e2a7170579fff77f145057bc56d72c99
Laize Ferraz <laizeferraz@gmail.com>
2021-01-12 21:26:25 +1100

    persisting data in the local storage.

---
+++ js/bookManager.js
@@ -0,0 +1,90 @@
+class BookManager {
+  // Create a constructor with a parameter currentid set to 0
+  constructor(currentId) {
+    // Initialize an empty array to save the books added 
+    this.books = []; 
+    // Set the currentId value to currentId
+    this.currentId = currentId || 0;
+  }
+
+  /*Add new books */
+
+  // Create a method with an object to add a book
+  addBook(title, author, language, date, status) {
+    const newBook = {
+      id: this.currentId++,
+      title,
+      author,
+      language,
+      date,
+      status,
+    };
+    // push the new book into the array 
+    this.books.push(newBook);
+    
+    /*Display list of books*/
+   
+    // Get the Javascript object new Date, give it the argument newBook.date, and assign it to a variable
+    const publishedDate = new Date(newBook.date);
+    // Format date to be dd/mm/yyyy
+    const formattedDate = publishedDate.getDate() + '/' + (publishedDate.getMonth() + 1) + '/' + publishedDate.getFullYear();
+
+    // Select the empty div from the HTML and assign it to a variable
+    const card = document.querySelector('#book-card');
+    // Create a new element <ul> in the html file and give it a class
+    const list = document.createElement('ul');
+    list.className ='list-group';
+    // Create an HTML card to display the new book added
+    list.innerHTML = `<li id=${newBook.id} class="list-group-item mt-2">
+                      <div class="d-flex w-100 mt-2 justify-content-between align-items-center">
+                        <h5>${newBook.title}</h5>
+                        <button type="button" class="btn delete-icon"></button>
+                      </div>
+                      <div class="d-flex w-100 mb-3 justify-content-between">
+                        <small>Author: ${newBook.author}</small>
+                        <small>Language: ${newBook.language}</small>
+                      </div>
+                      <div class="d-flex w-100 mt-3 justify-content-between align-items-center">
+                        <small>Published: ${formattedDate}</small>
+                        <span class="badge badge-status ${newBook.status === 'ToRead' ? 'badge-danger' : newBook.status === 'Reading' ? 'badge-warning' : newBook.status === 'Read' ? 'badge-success' : 'badge-primary'}">${newBook.status}</span>
+                      </div>
+                      <button onclick='changeToRead(this, ${newBook.id})' class="btn btn-outline-success done-button my-3 ${newBook.status === 'ToRead' || newBook.status === 'Reading' ? 'd-inline-block' : 'd-none' }">Mark As Read</button>
+                      <button onclick='changeToReading(this, ${newBook.id})' class="btn btn-outline-warning reading-button my-3 ${newBook.status === 'ToRead' ? 'd-inline-block' : 'd-none' }">Mark As Reading</button>
+                      </li> 
+          `; 
+    // Append the new HTML card to the empty div
+    card.appendChild(list);
+    
+  }
+  
+  save() {
+    if(localStorage.getItem('books')) {
+      localStorage.removeItem('books')
+    }
+    const booksJson = JSON.stringify(this.books);
+    localStorage.setItem('books', booksJson)
+  }
+  // delete(bookId) {
+  //   const newBook = [];
+  //   for(let i = 0; 1 < this.books.length; i++){
+  //     const book = this.books[i];
+  //     if(book.id !== bookId) {
+  //       newBook.push(book);
+  //     }
+  //   }
+  //   this.books = newBook;
+  // }
+
+  load() {
+   
+      const booksString = localStorage.getItem('books');
+      // this.books = JSON.parse(booksJson);
+      const booksJson = JSON.parse(booksString);
+      
+      booksJson.map(eachBook => {
+        const {title, author, language, date, status} = eachBook;
+        this.addBook(title, author, language, date, status);
+      });
+    }
+  }
+
---
+++ js/index.js
@@ -0,0 +1,89 @@
+// Initialize a new BookManager with current ID set to 0
+let bookManager = new BookManager();
+
+// Load book object from local storage 
+if(localStorage.getItem('books')) {
+  bookManager.load();
+} else {
+  bookManager = new BookManager(0);  
+};
+
+const newBookForm = document.querySelector('#book-form');
+
+newBookForm.addEventListener('submit', (event)=> {
+  event.preventDefault();
+
+  const newBookTitle = document.querySelector('#title');
+  const newBookAuthor = document.querySelector('#author');
+  const newBookLanguage = document.querySelector('#language');
+  const newBookDate = document.querySelector('#date');
+  const newBookStatus = document.querySelector('#status')
+  const errorMessage = document.querySelector('#alertMessage');
+
+  const title = newBookTitle.value;
+  const author = newBookAuthor.value;
+  const language = newBookLanguage.value;
+  const date = newBookDate.value;
+  const status = newBookStatus.value;
+
+  // Validation form
+  if(!validFormFieldInput(title)){
+    errorMessage.innerHTML = "Please write the book title.";
+    errorMessage.style.display = "block";
+  }else if (!validFormFieldInput(author)){
+    errorMessage.innerHTML = "Please write the author's name.";
+    errorMessage.style.display = "block";
+  }else if (!validFormFieldInput(language)) {
+    errorMessage.innerHTML = "Please put the language the book is writen.";
+    errorMessage.style.display = "block";
+  }else if (!validFormFieldInput(date)) {
+    errorMessage.innerHTML = "Please select the publishing date.";
+    errorMessage.style.display = "block";
+  }else if (!validFormFieldInput(status)) {
+    errorMessage.innerHTML = "Please select the reading status.";
+    errorMessage.style.display = "block";
+  }else {
+    errorMessage.style.display = "none";
+    // Add the new book to the book manager
+    bookManager.addBook(title, author, language, date, status);
+    // Save object in local storage
+    bookManager.save()
+    // Clear the form after submiting it correctly
+    event.target.reset();
+  };
+
+});
+
+const validFormFieldInput = (data) => data !== null && data !== '';
+
+const changeToRead = (button, id) => {
+  // Find the book id that matches the parent id
+     const book = bookManager.books.find(book => book.id === id);
+     book.status = 'Read';
+     bookManager.save();
+     const parentBook = button.parentElement;
+    //  If statement to garantee the changes on the UI matches the array of books
+      const badge = parentBook.getElementsByClassName('badge-status');
+        badge[0].classList.remove('badge-danger');
+        badge[0].classList.remove('badge-warning');
+        badge[0].classList.add('badge-success');/*  */
+        badge[0].innerHTML = `Read`;
+        button.remove();   
+}
+
+const changeToReading = (button, id) =>{
+// Find the book id that matches the parent id
+const book = bookManager.books.find(book => book.id === id);
+book.status = 'Reading';
+bookManager.save();
+const parentBook = button.parentElement;
+
+//  If statement to garantee the changes on the UI matches the array of books
+ const badge = parentBook.getElementsByClassName('badge-status');
+   badge[0].classList.remove('badge-danger');
+   badge[0].classList.add('badge-warning');
+   badge[0].innerHTML = `Reading`;
+   button.remove();
+}
+
+
